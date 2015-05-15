5d047719cf2292ea4966949ef1c3e754034eaeec
inyeoplee77 <blackin77@gmail.com>
2015-05-15 16:18:36 +0900

    new Movie DB

--- Movie_Data/parser.py
+++ Movie_Data/parser.py
@@ -2,22 +2,25 @@ import requests
 from BeautifulSoup import BeautifulSoup
 import re
 import sys
+from HTMLParser import HTMLParser
 reload(sys)
-
-
 sys.setdefaultencoding('utf8')
+parser = HTMLParser()
 
 urls = {}
 scraps = {}
 
 idx = 0
-#80001
-for x in range(1,82272,50):
-	urls[idx] = 'http://www.imdb.com/search/title?at=0&release_date=1992,2014&sort=num_votes&start='+str(x)+'&title=*&title_type=feature,short&user_rating=6.0,10'
-	scraps[idx] = BeautifulSoup(requests.get(urls[idx]).content.decode('utf-8','ignore')).find('table','results')
-	print urls[idx]
-	idx = idx + 1
-
+for x in range(1,6101,100):
+	while True:
+		try:
+			urls[idx] = 'http://www.imdb.com/search/title?count=100&countries=us&num_votes=5000,&sort=num_votes,desc&start='+str(x)+'&title_type=feature'
+			scraps[idx] = BeautifulSoup(requests.get(urls[idx]).content.decode('utf-8','ignore')).find('table','results')
+		except requests.ConnectionError:
+			continue
+		print urls[idx]
+		idx = idx + 1
+		break
 f = open('Movie_DB.txt','w')
 number = 0
 staff = {}
@@ -25,19 +28,15 @@ for i in scraps:
 	info_even = scraps[i].findAll('tr','even detailed')
 	info_odd = scraps[i].findAll('tr','odd detailed')
 	infos = info_odd + info_even
-
 	for info in infos:
 		
 		votes = info.find('td','sort_col')
-		
-		
-
-		info = info.find('td','title')
-		
+		info = info.find('td','title')		
 		staff = info.find('span','credit')
 		rating = info.find('span','value')
 		year = info.find('span','year_type')
 		genres = info.find('span','genre')
+		
 		if staff is None or rating is None or votes is None or genres is None or year is None:
 			continue
 
@@ -49,13 +48,14 @@ for i in scraps:
 		votes = votes.contents[0]
 		rating = rating.contents[0]
 		genres = genres.findAll('a')
-
 		directors = staff[staff.find('Dir:')+4:staff.find('With:')].split(',')
 		stars = staff[staff.find('With:')+5:].split(',')
-
 		title = info.find('a').contents[0]
 		
-
+		parser.unescape(title)
+		parser.unescape(directors)
+		parser.unescape(stars)
+		
 		f.write('title:'+title+'\n')
 		f.write('year:'+year+'\n')
 		f.write('rating:'+rating+'\n')
