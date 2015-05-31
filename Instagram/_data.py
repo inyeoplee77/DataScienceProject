d112c3198ecfba9b31765df060cc1016887ccc1f
jurim0301 <jurim0301@gmail.com>
2015-05-31 13:10:48 +0900

    vector

---
+++ Instagram/_data.py
@@ -0,0 +1,10 @@
+﻿import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+music_db = file('Music_DB.txt','r')
+movie_db = file('Movie_DB.txt','r')
+for a in music_db:
+    print a
+    print music_db.next()
--- Instagram/insta_music.py
+++ Instagram/insta_music.py
@@ -6,12 +6,12 @@ reload(sys)
 
 sys.setdefaultencoding('utf8')
 
-#access_token = '1085114028.334eb93.e4202257327a42fcbe2840b838ef4e55'
+access_token = '1085114028.334eb93.e4202257327a42fcbe2840b838ef4e55'
 #user_id = '1085114028'
 
 
 api = InstagramAPI(client_id='334eb938576d4207b655a2911944f919', client_secret='f734f47818ae462f9150d4816114df17')
-#api = InstagramAPI(access_token=access_token)
+api = InstagramAPI(access_token=access_token)
 
 users = {}
 
@@ -25,6 +25,7 @@ regex = re.compile('[^a-zA-Z]') #regualr expression for non-alphabets
 for line in user_db:
    tmp = line.split(':')
    users[tmp[0]] = tmp[1]
+
 
 for user in users:
    media, _next = api.user_recent_media(user)
@@ -42,77 +43,3 @@ for user in users:
 
 
 
-
-
-#ignore
-'''
-for line in music:
-   if 'title' not in line:
-      continue
-   titles = []
-   titles_drop_the = []
-   titles_drop_a = []
-   #extract keywords for searching tags from the title - ex)Harry Potter and the Deathly Hallows -> Harry Potter
-   
-   title = line.split('title:')[1]
-   title_original = title.strip()
-   title = title.lower().strip()
-   
-   titles.append(title)   
-   if ':' in title:
-      titles.extend(title.split(':'))
-   if 'and the' in title:
-      titles.extend(title.split('and the'))
-      
-   if title.find('the ') == 0 and 'and the' not in title:
-      for title in titles:
-         titles_drop_the.append(title.replace('the ',''))
-      titles.extend(titles_drop_the)
-   if title.find('a ') == 0:
-      for title in titles:
-         titles_drop_a.append(title.replace('a ',''))
-      titles.extend(titles_drop_a)
-   print title_original
-   #remove special characters
-   for i in range(len(titles)):
-      titles[i] = regex.sub('',titles[i])
-   #search movie title tags that have the most media
-   max_count = -1
-   for title in titles:
-      tags = api.tag_search(q=title,count = 10)
-      if not tags[0]:
-         continue
-      tags = tags[0]
-      count = tags[0].media_count
-      if count > max_count:
-         max_count = count
-         max_tag = tags[0]
-   users = {} #to avoid duplication, initialize users dictionary for every movie title
-   for j in range(10):
-      try:
-         media = api.tag_recent_media(tag_name = max_tag.name,)
-         if media[1] is None or max_count == -1:
-            continue
-         max_tag_id = media[1].split('max_tag_id=')[1]
-         print max_count     
-         #get ids of users who we want
-         num = 10 # number of iterations (= number of media)              
-         for i in range(num):
-            for m in media[0]:
-               if not hasattr(m,'tags'):
-                  continue
-               for tag in m.tags:
-                  if 'movie' in tag.name:
-                     user[m.user.id] = title_original
-            media = api.tag_recent_media(max_tag_id = max_tag_id,tag_name = max_tag.name)
-            if media[1] is None:
-               break
-            max_tag_id = media[1].split('max_tag_id=')[1]         
-      except Exception as e:
-         print e
-         continue
-      break
-   for key in users.keys():
-      user_db.write(str(key) + '::' + users[key] + '\n')        
-user_db.close()
-'''
