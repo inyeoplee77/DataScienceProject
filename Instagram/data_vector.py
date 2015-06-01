c97a7d677b8863a2fdd63ab2003caad99162e761
jurim0301 <jurim0301@gmail.com>
2015-06-01 16:32:07 +0900

    vector update

--- Instagram/data_vector.py
+++ Instagram/data_vector.py
@@ -23,33 +23,29 @@ for a in music_db:
         art_term = a.split(':')[1].split(',')
         for b in art_term:
             _data[b.strip()]=0
-    elif(a.split(':')[0]=='loudness'):
-        loudness = float(a.split(':')[1].strip())
-        if(loudness > max_loud):
-            max_loud = loudness
-        if(loudness < min_loud):
-            min_loud = loudness
-print (max_loud-min_loud)/10.0
-print min_loud
+
 for a in movie_db:
     if(a.split(':')[0]=='director'):
         director = a.split(':')[1].split(',')
         for b in director:
-            _data[b]=0
+            _data[b.strip()]=0
     elif(a.split(':')[0]=='stars'):
         stars = a.split(':')[1].split(',')
         for b in stars:
-            _data[b]=0
+            _data[b.strip()]=0
     elif(a.split(':')[0]=='genres'):
         genres = a.split(':')[1].split(',')
         for b in genres:
-            _data[b]=0
-    
+            _data[b.strip()]=0
+
+
 #making features which need to be separated into 10 parts
-for a in range(0,9):
+for a in range(0,10):
     string = 'energy:'+str(a)
     _data[string]=0
     string = 'liveness:'+str(a)
+    _data[string]=0
+    string = 'loudness:'+str(a)
     _data[string]=0
     string = 'tempo:'+str(a)
     _data[string]=0
@@ -59,20 +55,24 @@ for a in range(0,9):
     _data[string]=0
     string = 'danceability:'+str(a)
     _data[string]=0
-    string = 'instrumantalness:'+str(a)
+    string = 'instrumentalness:'+str(a)
     _data[string]=0
-    string = 'valence:'+str(a)
+    string = 'valeance:'+str(a)
     _data[string]=0
-    string = 'hotttnesss:'+str(a)
+    string = 'song_hotttnesss:'+str(a)
     _data[string]=0
     string = 'vote:'+str(a)
     _data[string]=0
     string = 'rate:'+str(a)
     _data[string]=0
 
-
+if '' in _data:
+    del _data['']
+if '\n' in _data:
+    del _data['\n']
+print len(_data)
 #search element(movie, music) in list
-title = ['See You Again','Thinking Out Loud']
+title = ['Inception','See You Again','Thinking Out Loud','The Hobbit: An Unexpected Journey','Star Wars: Episode VI - Return of the Jedi','No Country for Old Men','Hozier','Sugar','Elastic Heart']
 #format: title1 title2 title3 .... 
 for a in title:
     _data = dict.fromkeys(_data, 0)
@@ -86,20 +86,24 @@ for a in title:
             _line = movie_db.next()
             while(_line.split(':')[0].strip()!='title'):
                 if(_line.split(':')[0]=='rating'):
-                    string = 'rate'+str(int(float(_line.split(':')[1].replace('\n',''))))
-                    _data[string]=1
+                    string = 'rate:'+str(int(float(_line.split(':')[1].replace('\n',''))))
+                    if string.strip() in _data:
+                        _data[string.strip()]=1
                 elif(_line.split(':')[0]=='director'):
                     director = _line.split(':')[1].split(',')
                     for c in director:
-                        _data[c]=1
+                        if c.strip() in _data:
+                            _data[c.strip()]=1
                 elif(_line.split(':')[0]=='stars'):
                     stars = _line.split(':')[1].split(',')
                     for c in stars:
-                        _data[c]=1
+                        if c.strip() in _data:
+                            _data[c.strip()]=1
                 elif(_line.split(':')[0]=='genres'):
                     genres = _line.split(':')[1].split(',')
                     for c in genres:
-                        _data[c]=1
+                        if c.strip() in _data:
+                            _data[c.strip()]=1
                 _line = movie_db.next()
 
     #if a is song title
@@ -108,58 +112,79 @@ for a in title:
             _line = music_db.next()
             while(_line.split(':')[0].strip()!='title'):
                 if(_line.split(':')[0]=='artist'):
-                    _data[_line.split(':')[1].strip()]=1
+                    string = _line.split(':')[1].strip()
+                    if string in _data:
+                        _data[string]=1
                 elif(_line.split(':')[0]=='energy'):
                     ene = float(_line.split(':')[1].strip())
                     string = 'energy:'+(str)((int)(ene/0.1))
-                    _data[string]=1
+                    if string in _data:
+                        _data[string]=1
                 elif(_line.split(':')[0]=='liveness'):
                     live = float(_line.split(':')[1].strip())
                     string = 'liveness:'+(str)((int)(live/0.1))
-                    _data[string]=1
+                    if string in _data:
+                        _data[string]=1
                 elif(_line.split(':')[0]=='tempo'):
                     tempo = int(float(_line.split(':')[1].strip()))-65
                     tempo /= 15
                     string = 'tempo:'+str(tempo)
-                    _data[string]=1
+                    if string in _data:
+                        _data[string]=1
                 elif(_line.split(':')[0]=='speechiness'):
                     a = float(_line.split(':')[1].strip())
                     string = 'speechiness:'+(str)((int)(a/0.1))
-                    _data[string]=1
+                    if string in _data:
+                        _data[string]=1
                 elif(_line.split(':')[0]=='acousticness'):
                     a = float(_line.split(':')[1].strip())
                     string = 'acousticness:'+(str)((int)(a/0.1))
-                    _data[string]=1
+                    if string in _data:
+                        _data[string]=1
                 elif(_line.split(':')[0]=='danceability'):
                     a = float(_line.split(':')[1].strip())
                     string = 'danceability:'+(str)((int)(a/0.1))
-                    _data[string]=1
+                    if string in _data:
+                        _data[string]=1
                 elif(_line.split(':')[0]=='instrumentalness'):
                     a = float(_line.split(':')[1].strip())
                     string = 'instrumentalness:'+(str)((int)(a/0.1))
-                    _data[string]=1
+                    if string in _data:
+                        _data[string]=1
                 elif(_line.split(':')[0]=='loudness'):
                     a = float(_line.split(':')[1].strip())+31
                     string = 'loudness:'+str((int)(a/3.0))
-                    _data[string]=1
+                    if string in _data:
+                        _data[string]=1
                 elif(_line.split(':')[0]=='valence'):
                     a = float(_line.split(':')[1].strip())
                     string = 'valeance:'+(str)((int)(a/0.1))
-                    _data[string]=1
+                    if string in _data:
+                        _data[string]=1
                 elif(_line.split(':')[0]=='song_hotttnesss'):
                     a = float(_line.split(':')[1].strip())-0.67
                     string = 'song_hotttnesss:'+(str)((int)(a/0.03))
-                    _data[string]=1
+                    if string in _data:
+                        _data[string]=1
                 elif(_line.split(':')[0]=='song_type' or _line.split(':')[0]=='artist_terms'):
                     a = _line.split(':')[1].split(',')
                     for b in a:
-                        _data[b.strip()]=1
+                        if b.strip() in _data:
+                            _data[b.strip()]=1
                 _line = music_db.next()
+    if '\n' in _data:
+        del _data['\n']
+    if '' in _data:
+        del _data['']
     x.append(_data)
 
 #delete unnecessary features
-del _data['\n']
-del _data['']
+
 
 
 #x will be the list of vector
+#for a in x:
+    #print [name for name, value in a.items() if value == 1]
+    #print len(a)
+f = open('test.txt','w') 
+f.write(str(x[0]))
